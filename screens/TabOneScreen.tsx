58464f17809a82b862b3ead011e44ab648c84bb5
liinnjohansson <93linjoh@skola.boras.se>
2021-09-23 16:44:07 +0200

    try to use context

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
+import { FlatList, StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import {BistroContext} from "../contexts/BistroContext";
+import { useContext } from 'react';
+import { bistros } from "../data/bistroData";
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const { storedBistros } = useContext(BistroContext);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Tab ooo Oneeeee</Text>
+
+      <FlatList
+        data={bistros}
+        renderItem={({item}) => <Text style={styles.item}>{item.title}</Text>}
+      />
+
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
@@ -29,5 +38,10 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
