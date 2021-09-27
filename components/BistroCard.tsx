ebf673a73c754a3940d84001d9709c21444c1431
Emelie Brobergs <emebro1@gmail.com>
2021-09-27 09:59:37 +0200

    installed: npm install react-native-elements

---
+++ components/BistroCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Card } from "react-native-elements";
+
+const BistroCard = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default BistroCard
+
+const styles = StyleSheet.create({})
