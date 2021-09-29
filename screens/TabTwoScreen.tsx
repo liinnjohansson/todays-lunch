7ad0114ce2f139a518319d05dd768930dbd0610b
eminamuratspahic <55393321+eminamuratspahic@users.noreply.github.com>
2021-09-29 15:34:26 +0200

    Added borås city as inital Region

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import { Dimensions, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { View } from '../components/Themed';
 
 export default function TabTwoScreen() {
   return (
     <View>
-      <MapView style={styles.map} />
+      <MapView style={styles.map} 
+      initialRegion={{
+        latitude: 57.7206788,
+        longitude: 12.9414395,
+        latitudeDelta: 0.0022,
+        longitudeDelta: 0.0021,
+      }}/>
     </View>
   );
 }
