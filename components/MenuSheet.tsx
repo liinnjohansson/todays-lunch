a1d9caa7e799ad4e737036691564d7180f2bd4a5
liinnjohansson <93linjoh@skola.boras.se>
2021-09-29 15:30:48 +0200

    added "från" on price and alternated styles

--- components/MenuSheet.tsx
+++ components/MenuSheet.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import {
-  ScrollView, StyleSheet, Text, View
-} from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { BistroData } from "../data/bistroData";
 
 interface Props {
@@ -11,47 +9,42 @@ interface Props {
 export default function MenuSheet({ bistro }: Props) {
   return (
     <View style={styles.container}>
-    <ScrollView contentContainerStyle={styles.contentContainer}>
-      <View>
-        <Text style={[styles.font, styles.bistro]}>
-          {bistro?.title}
-        </Text>
-        <Text style={[styles.font, styles.timeAndPrice]}>
-          Serveras mellan:{" "}
-          {bistro?.lunchOfTheWeekDefault.monday?.lunchStart.toFixed(2)}{" "}
-          - {bistro?.lunchOfTheWeekDefault.monday?.lunchEnd.toFixed(2)}
-        </Text>
-        {bistro?.lunchOfTheWeekDefault.monday?.dishes.map(
-          (dish, index) => {
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        <View>
+          <Text style={[styles.font, styles.bistro]}>{bistro?.title}</Text>
+          <Text style={[styles.font, styles.timeAndPrice]}>
+            Serveras mellan:{" "}
+            {bistro?.lunchOfTheWeekDefault.monday?.lunchStart.toFixed(2)} -{" "}
+            {bistro?.lunchOfTheWeekDefault.monday?.lunchEnd.toFixed(2)}
+          </Text>
+          {bistro?.lunchOfTheWeekDefault.monday?.dishes.map((dish, index) => {
             return (
               <Text style={[styles.font, styles.course]} key={index}>
                 {dish}
               </Text>
             );
-          }
-        )}
-      </View>
-      <View>
-        <Text style={[styles.font, styles.timeAndPrice]}>
-          {bistro?.lunchOfTheWeekDefault.monday?.priceFrom}:-
-        </Text>
-      </View>
-    </ScrollView>
+          })}
+        </View>
+        <View>
+          <Text style={[styles.font, styles.timeAndPrice]}>
+            Från {bistro?.lunchOfTheWeekDefault.monday?.priceFrom}:-
+          </Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 2,
     alignItems: "center",
     justifyContent: "center",
     marginTop: 50,
-    marginBottom: 50,
   },
   contentContainer: {
     backgroundColor: "#fff",
-    width: 309,
-    minHeight: 300,
+    width: 260,
     opacity: 0.95,
     padding: 20,
     justifyContent: "space-between",
@@ -66,12 +59,12 @@ const styles = StyleSheet.create({
     paddingTop: 2,
   },
   timeAndPrice: {
-    fontSize: 18,
+    fontSize: 16,
     paddingTop: 15,
     paddingBottom: 5,
   },
   course: {
-    fontSize: 12,
+    fontSize: 11,
     paddingTop: 5,
     paddingBottom: 5,
   },
