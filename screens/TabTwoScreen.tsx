f2e548573487dfe04e1afd3bd7663efc70f1c9cf
eminamuratspahic <55393321+eminamuratspahic@users.noreply.github.com>
2021-09-30 11:37:44 +0200

    rendering out every restaurant to map

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,30 +1,45 @@
-import * as React from 'react';
-import { Dimensions, StyleSheet } from 'react-native';
-import { View } from '../components/Themed';
+import * as React from "react";
+import { Dimensions, StyleSheet } from "react-native";
+import { View } from "../components/Themed";
 //Provider_google måste finnas för att stödja ios enheter
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
-import { mapStyle } from '../data/mapStyle';
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import { mapStyle } from "../data/mapStyle";
+import { BistroData } from "../data/bistroData";
+import { BistroContext } from "../contexts/BistroContext";
+import { useContext } from "react";
 
-export default function TabTwoScreen() {
+export default function TabTwoScreen(bistro: BistroData) {
+  const { storedBistros } = useContext(BistroContext);
+
   return (
     <View>
-      <MapView style={styles.map} 
-      initialRegion={{
-        latitude: 57.7206788,
-        longitude: 12.9414395,
-        latitudeDelta: 0.0022,
-        longitudeDelta: 0.0021,
-        
-      }}
-      provider={PROVIDER_GOOGLE}
-      customMapStyle={mapStyle}/>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        customMapStyle={mapStyle}
+        style={styles.map}
+        region={{
+          latitude: 57.7206788,
+          longitude: 12.9414395,
+          latitudeDelta: 0.0022,
+          longitudeDelta: 0.0021,
+        }}
+      >
+        {storedBistros.map((bistro) => (
+          <Marker
+            coordinate={{
+              latitude: bistro.address.latitude,
+              longitude: bistro.address.longitude,
+            }}
+          ></Marker>
+        ))}
+      </MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
