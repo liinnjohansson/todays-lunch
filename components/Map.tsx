9b6929484329351c25210ea7dbfbd7ecf64d046d
eminamuratspahic <55393321+eminamuratspahic@users.noreply.github.com>
2021-10-01 16:32:58 +0200

    Added function to tapped marker, moved out the component to own file in components.

---
+++ components/Map.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import { Dimensions, StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import { mapStyle } from "../data/mapStyle";
+import { BistroData } from "../data/bistroData";
+import { BistroContext } from "../contexts/BistroContext";
+import { useContext, useState } from "react";
+import {
+View, Image
+} from "react-native";
+
+export default function Map() {
+  const [selectedId, setSelectedId] = useState<string>()
+  const { storedBistros } = useContext(BistroContext);
+  const marker = require('../images/icons/marker.png');
+  const selectedMarker = require('../images/icons/pressed-marker.png')
+
+  return (
+    <View>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        customMapStyle={mapStyle}
+        style={styles.map}
+        initialRegion={{
+          latitude: 57.7206788,
+          longitude: 12.9414395,
+          latitudeDelta: 0.0022,
+          longitudeDelta: 0.0021,
+        }}
+      >
+        {storedBistros.map((bistro, i) => (
+          <Marker
+            key={bistro.id}
+            coordinate={{
+              latitude: bistro.address.latitude,
+              longitude: bistro.address.longitude,
+            }}
+            title={bistro.title}
+            description={bistro.address.streetAddress}
+            onPress={() => setSelectedId(bistro.id)}
+          > 
+          <Image source={selectedId === bistro.id ? selectedMarker : marker}
+          />             
+          </Marker>
+        ))}
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  }
+});
