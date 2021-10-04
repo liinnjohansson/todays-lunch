4f42d0187abe9192e416ac77f61945ca67d95e08
eminamuratspahic <55393321+eminamuratspahic@users.noreply.github.com>
2021-10-04 14:56:09 +0200

    refactoring code

--- App.tsx
+++ App.tsx
@@ -2,29 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import BistroProvider from './contexts/BistroContext';
-
-// TODO: Fråga david om det är ok att ta bort detta bortkommenterade 
-// import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import AnimatedAppLoader from './screens/SplashScreen';
+import AnimatedSplashScreen from './screens/SplashScreen';
 
 export default function App() {
-  // const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
-  // if (!isLoadingComplete) {
-  //   return null;
-  // } else {
     return (
       <SafeAreaProvider>
         <BistroProvider>
-        <AnimatedAppLoader>
+        <AnimatedSplashScreen>
         <Navigation colorScheme={colorScheme} />
-        </AnimatedAppLoader>
+        </AnimatedSplashScreen>
         <StatusBar />
       </BistroProvider>
       </SafeAreaProvider>
     );
   }
-// }
--- screens/SplashScreen.tsx
+++ screens/SplashScreen.tsx
@@ -7,15 +7,10 @@ import {
   View,
 } from "react-native";
 
-// Instruct SplashScreen not to hide yet, we want to do this manually
 SplashScreen.preventAutoHideAsync().catch(() => {
 });
 
-export default function AnimatedAppLoader({ children }: any) {
-  return <AnimatedSplashScreen>{children}</AnimatedSplashScreen>;
-}
-
-function AnimatedSplashScreen({ children }: any) {
+export default function AnimatedSplashScreen({ children }: any) {
   const animation = React.useMemo(() => new Animated.Value(1), []);
   const [isAppReady, setAppReady] = React.useState(false);
   const [isSplashAnimationComplete, setAnimationComplete] = React.useState(
@@ -35,10 +30,10 @@ function AnimatedSplashScreen({ children }: any) {
   async function loadResourcesAndDataAsync() {
     try {
       await SplashScreen.hideAsync();
-      // Load stuff
+      
       await Promise.all([]);
     } catch (e) {
-      // handle errors
+      
     } finally {
       setAppReady(true);
     }
@@ -55,7 +50,6 @@ function AnimatedSplashScreen({ children }: any) {
             {
               backgroundColor: Constants.manifest?.splash?.backgroundColor,
               opacity: animation,
-              
             },
           ]}
         >
@@ -63,7 +57,6 @@ function AnimatedSplashScreen({ children }: any) {
             style={{
               width: "100%",
               height: "100%",
-              // resizeMode: Constants.manifest.splash.resizeMode || "center",
               transform: [
                 {
                   scale: animation,
