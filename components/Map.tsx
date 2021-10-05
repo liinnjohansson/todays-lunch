db2aff0077fcf990694fb871919e16d1b27cdb25
eminamuratspahic <55393321+eminamuratspahic@users.noreply.github.com>
2021-10-05 18:14:38 +0200

    changed accuracy

--- components/Map.tsx
+++ components/Map.tsx
@@ -29,10 +29,10 @@ export default function Map() {
             setErrorMsg('Permission to access location was denied');
             return;
           }
-          let location = await Location.getCurrentPositionAsync({accuracy: 1});
+          let location = await Location.getCurrentPositionAsync({accuracy:Location.Accuracy.Low});
           setUserLocation(location.coords);
-          // location = await Location.getCurrentPositionAsync({accuracy:Location.Accuracy.High});
-          // setUserLocation(location.coords);
+          location = await Location.getCurrentPositionAsync({accuracy:Location.Accuracy.High});
+          setUserLocation(location.coords);
         })();
       }, []);
 
