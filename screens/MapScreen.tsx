7b115fdce23e524419272f53d2bb5fe858766203
eminamuratspahic <55393321+eminamuratspahic@users.noreply.github.com>
2021-10-05 19:20:02 +0200

    Added wronlgy deleted code in mergeconflict

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -5,14 +5,22 @@ import { StyleSheet } from "react-native";
 import { View } from "../components/Themed";
 import { RootStackScreenProps } from "../navigation/RootStackNavigator";
 import { TabScreenProps } from "../navigation/TabBistroMapNavigator";
+import { BistroContext } from "../contexts/BistroContext";
+import MapInfoBox from "../components/MapInfoBox";
+import { useContext } from "react";
 
 // CompositScreenProp används för att nvigera till vilken sida som helst från denna sida,
 // Taben bahövs...för route.params inom denna fil
 type Props = CompositeScreenProps<TabScreenProps<"Map">, RootStackScreenProps>;
 
-export default function MapScreen({ navigation }: Props) {
+export default function MapScreen({ navigation, route }: Props) {
+const id = "1";
+const { storedBistros } = useContext(BistroContext);
+const selectedBistro = storedBistros.find((bistro) => bistro.id === id);
+
   return (
     <View style={styles.container}>
+      {selectedBistro && <MapInfoBox bistro={selectedBistro} />}
           <Map/>
     </View>
   );
@@ -21,5 +29,19 @@ export default function MapScreen({ navigation }: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
