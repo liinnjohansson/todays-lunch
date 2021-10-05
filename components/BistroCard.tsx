61583e3948bf696fe80c5d9c89723ce8c335c567
Emelie Brobergs <emebro1@gmail.com>
2021-10-05 20:14:43 +0200

    added option emprt [] on LunchOfTheWeekOffer

--- components/BistroCard.tsx
+++ components/BistroCard.tsx
@@ -14,30 +14,33 @@ import LikeButton from "./LikeButton";
 interface Props {
   bistro: BistroData;
   weekday: "monday" | "tuesday" | "wednesday" | "thursday" | "friday";
+  weekNumber: number;
 }
 
 //TODO: Lägg till prop för onPress som hanterar navigation i Screen sidan
 
-const BistroCard = ({ bistro, weekday }: Props) => {
+const BistroCard = ({ bistro, weekday, weekNumber }: Props) => {
   const image = { uri: `${bistro.imageUrl}` };
   const title = bistro.title;
   let lunchStart: String | undefined;
   let lunchEnd: String | undefined;
   let priceFrom: Number | undefined;
 
-  const week = 20;
-
   // TODO: Write code that check if weeknumer exist in the server thath correspond to requested week on app screen
   // Mock data for this case are written for bistro: The Company
   // This code maybe cut to its own file, and reused ?
 
-  const lunchOfTheWeek = bistro.lunchOfTheWeekOffer?.find(
-    (lunch) => lunch.weekNumber == week
-  );
-
-  lunchStart = bistro.lunchOfTheWeekDefault[weekday]?.lunchStart?.toFixed(2);
-  lunchEnd = bistro.lunchOfTheWeekDefault[weekday]?.lunchEnd?.toFixed(2);
-  priceFrom = bistro.lunchOfTheWeekDefault[weekday]?.priceFrom;
+  if (
+    bistro.lunchOfTheWeekOffer?.find((lunch) => lunch.weekNumber == weekNumber)
+  ) {
+    lunchStart = bistro.lunchOfTheWeekOffer[weekday]?.lunchStart?.toFixed(2);
+    lunchEnd = bistro.lunchOfTheWeekOffer[weekday]?.lunchEnd?.toFixed(2);
+    priceFrom = bistro.lunchOfTheWeekOffer[weekday]?.priceFrom;
+  } else {
+    lunchStart = bistro.lunchOfTheWeekDefault[weekday]?.lunchStart?.toFixed(2);
+    lunchEnd = bistro.lunchOfTheWeekDefault[weekday]?.lunchEnd?.toFixed(2);
+    priceFrom = bistro.lunchOfTheWeekDefault[weekday]?.priceFrom;
+  }
 
   // skicka tillbaka ett lunch objekt
 
