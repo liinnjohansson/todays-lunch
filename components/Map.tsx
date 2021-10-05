8b0da31d91d7803363bd8815366e16b9a7059dfd
eminamuratspahic <55393321+eminamuratspahic@users.noreply.github.com>
2021-10-05 16:32:02 +0200

    Added user location and code improving

--- components/Map.tsx
+++ components/Map.tsx
@@ -3,21 +3,47 @@ import { Dimensions, StyleSheet } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import { mapStyle } from "../data/mapStyle";
 import { BistroContext } from "../contexts/BistroContext";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import * as Location from 'expo-location';
 import {
 View, Image
 } from "react-native";
-import Permission from "./Permission";
+
+interface GeoLocation {
+  longitude: number,
+  latitude: number,
+}
+
+interface MarkerLocation {
+  longitude: number,
+  latitude: number,
+
+}
 
 export default function Map() {
   const [selectedId, setSelectedId] = useState<string>()
   const { storedBistros } = useContext(BistroContext);
   const marker = require('../images/icons/marker.png');
-  const selectedMarker = require('../images/icons/pressed-bistro-marker.png')
-  
-  const [location, setLocation] = useState();
-  const [errorMsg, setErrorMsg] = useState();
+  const selectedMarkerImage = require('../images/icons/pressed-bistro-marker.png')
+  const [userLocation, setUserLocation] = useState<GeoLocation>();
+  const [errorMsg, setErrorMsg] = useState<string>();  
+  const [selectedMarker, setSelectedMarker] = useState<MarkerLocation>();
+
+  //subscription 
+
+    useEffect(() => {
+        (async () => {
+          const { status } = await Location.requestForegroundPermissionsAsync();
+          if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+            return;
+          }
+          let location = await Location.getCurrentPositionAsync({accuracy:Location.Accuracy.Low});
+          setUserLocation(location.coords);
+          location = await Location.getCurrentPositionAsync({accuracy:Location.Accuracy.High});
+          setUserLocation(location.coords);
+        })();
+      }, []);
 
   return (
     <View>
@@ -26,14 +52,22 @@ export default function Map() {
         customMapStyle={mapStyle}
         style={styles.map}
         initialRegion={{
-          latitude: 57.7206788,
-          longitude: 12.9414395,
+          latitude: userLocation?.latitude || 57.7206788,
+          longitude: userLocation?.longitude || 57.7206788,
           latitudeDelta: 0.0022,
           longitudeDelta: 0.0021,
         }}
       >
-              <Permission/>
-        {storedBistros.map((bistro, i) => (
+        <Marker
+        coordinate={{
+          latitude: userLocation?.latitude || 57.7206788,
+          longitude: userLocation?.longitude || 57.7206788,
+             }}>
+               <Image
+               style={styles.image}
+               source={require('../images/icons/cat.png')}    /> 
+          </Marker>
+        {storedBistros.map((bistro) => (
           <Marker
             key={bistro.id}
             coordinate={{
@@ -42,9 +76,15 @@ export default function Map() {
             }}
             title={bistro.title}
             description={bistro.address.streetAddress}
-            onPress={() => setSelectedId(bistro.id)}
+            onPress={() =>  {
+              setSelectedMarker({
+                latitude: bistro.address.latitude,
+                longitude: bistro.address.longitude,
+              }); 
+              setSelectedId(bistro.id)
+            }}
           > 
-          <Image source={selectedId === bistro.id ? selectedMarker : marker}
+          <Image source={selectedId === bistro.id ? selectedMarkerImage : marker}
           />             
           </Marker>
         ))}
@@ -57,5 +97,9 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
+  },
+  image: {
+    width: 50,
+    height: 100
   }
 });
