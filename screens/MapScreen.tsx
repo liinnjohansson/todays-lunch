4a1094b371a66529100a2d1043160e85a9a752d0
Emelie Brobergs <emebro1@gmail.com>
2021-10-06 15:10:18 +0200

    deleted comments

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,27 +1,17 @@
 import { CompositeScreenProps } from "@react-navigation/native";
 import * as React from "react";
-import Map from '../components/Map';
+import Map from "../components/Map";
 import { StyleSheet } from "react-native";
 import { View } from "../components/Themed";
 import { RootStackScreenProps } from "../navigation/RootStackNavigator";
 import { TabScreenProps } from "../navigation/TabBistroMapNavigator";
-// import { BistroContext } from "../contexts/BistroContext";
-// import MapInfoBox from "../components/MapInfoBox";
-// import { useContext } from "react";
 
-// CompositScreenProp används för att nvigera till vilken sida som helst från denna sida,
-// Taben bahövs...för route.params inom denna fil
 type Props = CompositeScreenProps<TabScreenProps<"Map">, RootStackScreenProps>;
 
 export default function MapScreen({ navigation, route }: Props) {
-// const id = "1";
-// const { storedBistros } = useContext(BistroContext);
-// const selectedBistro = storedBistros.find((bistro) => bistro.id === id);
-
   return (
     <View style={styles.container}>
-      {/* {selectedBistro && <MapInfoBox bistro={selectedBistro} />} */}
-          <Map/>
+      <Map />
     </View>
   );
 }
@@ -29,5 +19,5 @@ export default function MapScreen({ navigation, route }: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  }
+  },
 });
