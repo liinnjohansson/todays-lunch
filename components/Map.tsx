359efbce1f55036474e116d5d05929084fec6c24
eminamuratspahic <55393321+eminamuratspahic@users.noreply.github.com>
2021-10-06 20:21:56 +0200

    Added react-native-maps-directions

--- components/Map.tsx
+++ components/Map.tsx
@@ -4,31 +4,49 @@ import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import { mapStyle } from "../data/mapStyle";
 import { BistroContext } from "../contexts/BistroContext";
 import { useContext, useState } from "react";
-import {
-View, Image
-} from "react-native";
+import { Image } from "react-native";
 import { MapContext } from "../contexts/MapContext";
+import MapViewDirections from 'react-native-maps-directions';
 
 function Map() {
   const [selectedId, setSelectedId] = useState<string>()
   const { storedBistros } = useContext(BistroContext);
   const marker = require('../images/icons/marker.png');
   const selectedMarkerImage = require('../images/icons/pressed-bistro-marker.png')
-  const { userLocation} = useContext(MapContext); 
+  const { userLocation } = useContext(MapContext); 
+  const GOOGLE_MAPS_APIKEY = 'AIzaSyBvSXOW7pC6kk7InV59oBFOCQ8WZiBUTz0';
+  
 
   return (
-    <View>
       <MapView
         provider={PROVIDER_GOOGLE}
         customMapStyle={mapStyle}
         style={styles.map}
         initialRegion={{
-          latitude: userLocation?.latitude || 57.7206788,
-          longitude:  userLocation?.longitude|| 57.7206788,
+          latitude: userLocation?.latitude || 12.938943407626931,
+          longitude:  userLocation?.longitude|| 12.938943407626931,
           latitudeDelta: 0.0022,
           longitudeDelta: 0.0021,
         }}
+        
       >
+        <MapViewDirections
+    origin={{
+      latitude: userLocation?.latitude || 57.72002214821148,
+      longitude:  userLocation?.longitude|| 12.938943407626931,
+    }}
+    destination={{
+      latitude: 57.72002214821148,
+      longitude: 12.938943407626931,
+    }}
+    apikey={GOOGLE_MAPS_APIKEY}
+    strokeWidth={3}
+    strokeColor="red"
+    onReady={result => {
+      console.log(`Distance: ${result.distance} km`)
+      console.log(`Duration: ${result.duration} min.`)
+    }}
+  />
         <Marker
         coordinate={{
           latitude: userLocation?.latitude || 57.7206788,
@@ -42,23 +60,24 @@ function Map() {
           <Marker
             key={bistro.id}
             coordinate={{
-              latitude: bistro.address.latitude,
-              longitude: bistro.address.longitude,
+              latitude: bistro.address.latitude || 57.7206788,
+              longitude: bistro.address.longitude || 57.7206788,
             }}
             title={bistro.title}
             description={bistro.address.streetAddress}
-            onPress={() =>  
-              setSelectedId(bistro.id)
+            onPress={() =>  {
+              setSelectedId(bistro.id)}
             }
+            
           > 
           <Image source={selectedId === bistro.id ? selectedMarkerImage : marker}
-          />             
+          />            
           </Marker>
         ))}
       </MapView>
-    </View>
   );
 }
+
 
 export default Map;
 
